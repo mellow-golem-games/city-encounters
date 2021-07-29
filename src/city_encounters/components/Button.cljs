da88213115466d3b999d66bf789dc8b512115260
peterjewicz <peterjewicz@totalwebconnections.com>
2021-07-29 15:31:52 -0500

    Set up base styles

--- src/city_encounters/components/Button.cljs
+++ src/city_encounters/components/Button.cljs
@@ -3,5 +3,5 @@
 ; This is a fake button we made just for this app to make some styling stuff easier.
 
 (defn Button [text active? on-click]
-  [:div.Button {:class (if active? "active" nil) :on-click #(on-click)}
+  [:div.Button.rounded.py-2.px-4 {:class (if active? "active" nil) :on-click #(on-click)}
    [:p text]])
--- src/city_encounters/views/home.cljs
+++ src/city_encounters/views/home.cljs
@@ -14,6 +14,10 @@
   (re-frame/dispatch [:set-is-loading false])
   (re-frame/dispatch [:set-encounter (:body res)]))
 
+(defn on-error []
+  (re-frame/dispatch [:set-is-loading false])
+  (js/alert "There was an error"))
+
 (defn get-encounter [size outcome extra]
   (re-frame/dispatch [:set-is-loading true])
   (let [outcomes-string (subs (reduce #(str %1 "," %2) "" (concat [outcome] extra)) 1)
@@ -22,7 +26,7 @@
       (fn [res]
         (if (= (:status res) 200)
           (on-success res)
-          (js/alert "There was an erorr"))))))
+          (on-error))))))
 
 (defn set-current-size [size-string]
   (re-frame/dispatch [:set-current-size size-string]))
@@ -41,21 +45,23 @@
         current-outcome @(re-frame/subscribe [:current-outcome])
         extra-outcomes @(re-frame/subscribe [:extra-outcomes])
         is-loading? @(re-frame/subscribe [:is-loading])]
-    [:div.Home.page
-     [:h1 "Home"]
-     [:p current-size]
-     [:div.Home__buttonWrapper
+    [:div.Home.page.text-center.container.mx-auto.pt-4.px-2
+     [:h2.text-4xl.pb-2 "Choose Your Encounter Settings"]
+     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+      [:h3.min-w-full.text-3xl.pb-2 "Location Size"]
       (for [size SIZES]
         ^{:key size} [Button size (= current-size size) #(set-current-size size)])]
-     [:div.Home__buttonWrapper
+     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+      [:h3.min-w-full.text-3xl.pb-2 "Outcome"]
       (for [outcome OUTCOME_TYPE]
         ^{:key outcome} [Button outcome (= current-outcome outcome) #(set-current-outcome outcome)])]
-     [:div.Home__buttonWrapper
+     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+      [:h3.min-w-full.text-3xl.pb-2 "Extras"]
       (for [outcome OUTCOMES]
         (let [is-in-list? (some #(= outcome %) extra-outcomes)]
           ^{:key outcome} [Button outcome is-in-list? #(set-extra-outcomes outcome (not is-in-list?))]))]
 
-     [:button {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
+     [:button.text-xl.rounded.py-2.px-4.uppercase {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
      (if is-loading?
        [Loader]
        (if encounter
