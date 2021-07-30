86e8ed8c925cf101926f04dd249b0334bcdc975f
peterjewicz <peterjewicz@totalwebconnections.com>
2021-07-30 15:38:37 -0400

    Dropdown and layout styles

--- src/city_encounters/views/home.cljs
+++ src/city_encounters/views/home.cljs
@@ -45,27 +45,31 @@
         current-outcome @(re-frame/subscribe [:current-outcome])
         extra-outcomes @(re-frame/subscribe [:extra-outcomes])
         is-loading? @(re-frame/subscribe [:is-loading])]
-    [:div.Home.page.text-center.container.mx-auto.pt-4.px-2
-     [:h2.text-4xl.pb-2 "Choose Your Encounter Settings"]
-     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
-      [:h3.min-w-full.text-3xl.pb-2 "Location Size"]
-      (for [size SIZES]
-        ^{:key size} [Button size (= current-size size) #(set-current-size size)])]
-     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
-      [:h3.min-w-full.text-3xl.pb-2 "Outcome"]
-      (for [outcome OUTCOME_TYPE]
-        ^{:key outcome} [Button outcome (= current-outcome outcome) #(set-current-outcome outcome)])]
-     [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
-      [:h3.min-w-full.text-3xl.pb-2 "Extras"]
-      (for [outcome OUTCOMES]
-        (let [is-in-list? (some #(= outcome %) extra-outcomes)]
-          ^{:key outcome} [Button outcome is-in-list? #(set-extra-outcomes outcome (not is-in-list?))]))]
-
-     [:button.text-xl.rounded.py-2.px-4.uppercase {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
+    [:div.Home.page.text-center.mx-auto.pt-4 {:class (if encounter "Home__encounter" "")}
+     [:div.Home__options.px-2
+      [:h2.text-4xl.pb-2 "Choose Your Encounter Settings"]
+      [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+       [:h3.min-w-full.text-3xl.pb-2 "Location Size"]
+       (for [size SIZES]
+         ^{:key size} [Button size (= current-size size) #(set-current-size size)])]
+      [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+       [:h3.min-w-full.text-3xl.pb-2 "Outcome"]
+       (for [outcome OUTCOME_TYPE]
+         ^{:key outcome} [Button outcome (= current-outcome outcome) #(set-current-outcome outcome)])]
+      [:div.Home__buttonWrapper.flex.flex-wrap.py-4.justify-center
+       [:h3.min-w-full.text-3xl.pb-2 "Extras"]
+       (for [outcome OUTCOMES]
+         (let [is-in-list? (some #(= outcome %) extra-outcomes)]
+           ^{:key outcome} [Button outcome is-in-list? #(set-extra-outcomes outcome (not is-in-list?))]))]
+      [:button.text-xl.rounded.py-2.px-4.uppercase {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
+      [:div.Home__options__bottom.text-center.flex.items-center.flex-col.justify-center {:on-click #(re-frame/dispatch [:set-encounter nil])}
+       [:h3.text-white.text-xl.flex-grow-0  "New Encounter?"]
+       [:svg {:aria-hidden "true" :focusable "false" :role "img" :style {:width "15px"} :xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 320 512"}
+         [:path {:fill "white" :d "M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"}]]]]
      (if is-loading?
        [Loader]
        (if encounter
-         [:div
+         [:div.Home__results
           [:p (:name encounter)]
           [:p (:description encounter)]]
          nil))]))
