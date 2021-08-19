552bae875e0bb04b9f5945a2042772a816b21fec
peterjewicz <peterjewicz@totalwebconnections.com>
2021-08-18 21:15:06 -0500

    Base button styles

--- src/city_encounters/components/Button.cljs
+++ src/city_encounters/components/Button.cljs
@@ -3,5 +3,8 @@
 ; This is a fake button we made just for this app to make some styling stuff easier.
 
 (defn Button [text active? on-click]
-  [:div.Button.rounded.py-2.px-4 {:class (if active? "active" nil) :on-click #(on-click)}
+  [:div.Button.text-white {:class (if active? "active" nil) :on-click #(on-click)
+                           :style {:backgroundImage (if-not active? "url('/images/button.png')" "url('/images/button_active.png')")}}
+
+   ; [:img {:src "/images/button.png"}]
    [:p text]])
