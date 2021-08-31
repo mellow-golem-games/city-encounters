52dfa4f54cc98b896f0acb55e6d9b1ebaa75ab84
peterjewicz <peterjewicz@totalwebconnections.com>
2021-08-31 11:55:28 -0500

    Move to capacitor for IOS deploys

--- mobile/www/js/compiled/cljs-runtime/city_encounters.config.js
+++
@@ -1,8 +0,0 @@
-goog.provide('city_encounters.config');
-city_encounters.config.debug_QMARK_ = goog.DEBUG;
-/**
- * @define {string}
- */
-city_encounters.config.API_URL = goog.define("city_encounters.config.API_URL","http://localhost:8080/");
-
-//# sourceMappingURL=city_encounters.config.js.map
--- mobile/www/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { city_encounters.core.init(); } catch (e) { console.error("An error occurred when calling (city-encounters.core/init)"); throw(e); }
