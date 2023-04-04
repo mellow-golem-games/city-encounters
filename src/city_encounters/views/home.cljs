4df58e1f61c27e40a13b7042b60910b3dbf25f0b
peterjewicz <peterjewicz@totalwebconnections.com>
2023-04-04 08:40:37 -0500

    Fix view on smaller screens

--- src/city_encounters/views/home.cljs
+++ src/city_encounters/views/home.cljs
@@ -96,25 +96,26 @@
      [:div.Home__options.px-2
       [:div.Header
        [:h2.text-2xl.md:text-4xl.pb-2.pt-2 "Choose Your Settings"]]
-      [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
-       [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Location Size"]
-       (for [size SIZES]
-         ^{:key size} [Button size (= current-size size) #(set-current-size size)])]
-      [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
-       [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Outcome"]
-       (for [outcome OUTCOME_TYPE]
-         ^{:key outcome} [Button outcome (= current-outcome outcome) #(set-current-outcome outcome)])]
-      [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
-       [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Extras (Optional)"]
-       (for [outcome OUTCOMES]
-         (let [is-in-list? (some #(= outcome %) extra-outcomes)]
-           ^{:key outcome} [Button outcome is-in-list? #(set-extra-outcomes outcome (not is-in-list?))]))]
-      [:button.Button.generate.text-xl.text-white.rounded.uppercase.mt-4 {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
-      [:div.Home__options__bottom.text-center.flex.items-center.flex-col.justify-center {:on-click #(on-encounter-reset)}
-       [:h3.text-white.text-xl.flex-grow-0  "New Encounter?"]
-       [:svg {:aria-hidden "true" :focusable "false" :role "img" :style {:width "15px"} :xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 320 512"}
-        [:path {:fill "white" :d "M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"}]]
-       [:div.Home__options__bottom__border]]]
+      [:div.Home__contentWrapper
+       [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
+        [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Location Size"]
+        (for [size SIZES]
+          ^{:key size} [Button size (= current-size size) #(set-current-size size)])]
+       [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
+        [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Outcome"]
+        (for [outcome OUTCOME_TYPE]
+          ^{:key outcome} [Button outcome (= current-outcome outcome) #(set-current-outcome outcome)])]
+       [:div.Home__buttonWrapper.flex.flex-wrap.py-2.md:py-4.justify-center
+        [:h3.min-w-full.text-2xl.md:text-3xl.pb-2 "Extras (Optional)"]
+        (for [outcome OUTCOMES]
+          (let [is-in-list? (some #(= outcome %) extra-outcomes)]
+            ^{:key outcome} [Button outcome is-in-list? #(set-extra-outcomes outcome (not is-in-list?))]))]
+       [:button.Button.generate.text-xl.text-white.rounded.uppercase.mt-4 {:on-click #(get-encounter current-size current-outcome extra-outcomes)} "Generate"]
+       [:div.Home__options__bottom.text-center.flex.items-center.flex-col.justify-center {:on-click #(on-encounter-reset)}
+        [:h3.text-white.text-xl.flex-grow-0  "New Encounter?"]
+        [:svg {:aria-hidden "true" :focusable "false" :role "img" :style {:width "15px"} :xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 320 512"}
+         [:path {:fill "white" :d "M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"}]]
+        [:div.Home__options__bottom__border]]]]
      (if is-loading?
        [Loader]
        (if encounter
